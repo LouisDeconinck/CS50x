dbcccdbc203552b28d50762f4a4f82510f59f9d8
Louis Deconinck <louis.dck@gmail.com>
2022-01-10 14:56:11 +0100

    Create mario-less.c

---
+++ Week 1: C/mario-less.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Ask for pyramid height
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // Variable to store amount of space in front
+    int spaces = height - 1;
+
+    // Loop through each level of the pyramid
+    for (int i = 0; i < height; i++)
+    {
+        // Spaces in front
+        for (int j = 0; j < spaces; j++)
+        {
+            printf(" ");
+        }
+        spaces--;
+
+        // Pyramid
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        // To next level
+        printf("\n");
+    }
+}
